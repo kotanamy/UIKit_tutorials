ee5e0a08c1cec344adab5aed7016c51646dc92db
kotanamy <donat0009@outlook.com>
2022-08-14 17:45:51 +0300

    init

---
+++ tutorial1/tutorial1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tutorial1
+//
+//  Created by Ярослав Котов on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
---
+++ tutorial1/tutorial1/ViewController2.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController2.swift
+//  tutorial1
+//
+//  Created by Ярослав Котов on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
